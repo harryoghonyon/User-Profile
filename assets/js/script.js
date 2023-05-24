509d87102ec97b1bcde486ed075422a92895a370
Harry Oghonyon <harrytones9@gmail.com>
2023-05-25 00:04:25 +0100

    add elements
    add live time and date, add new profile button and empty profile img

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 // Get the current date and time
 let currentDate = new Date();
 
+// Get the current day as an index (0-6)
+let currentDayIndex = currentDate.getDay();
+
+// Array of day names
+let dayNames = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+// Get the current day name
+let currentDayName = dayNames[currentDayIndex];
+
+// Update the element with the current day
+let todayElement = document.querySelector(".today-date");
+todayElement.textContent = currentDayName;
+
 // Get the hour and minute values
 let currentHour = currentDate.getHours();
 let currentMinute = currentDate.getMinutes();
@@ -12,3 +25,20 @@ hourElement.textContent = currentHour;
 // Update the minute element
 let minuteElement = document.querySelector(".mins-live-time");
 minuteElement.textContent = currentMinute;
+
+// Update the seconds element
+const updateSecs = () => {
+  // Get the current date and time
+  let currentDate = new Date();
+
+  // Get the second value
+  let currentSecs = currentDate.getSeconds();
+
+  // Update the seconds element
+  let secondsElement = document.querySelector(".secs-live-time");
+  secondsElement.textContent = currentSecs;
+};
+
+updateSecs();
+
+setInterval(updateSecs, 1000);
