818076434ea6786a5301a1c8a0b571d5418c21f7
Harry Oghonyon <harrytones9@gmail.com>
2023-05-25 19:40:36 +0100

    add form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,23 @@
-// Get the current date and time
 let currentDate = new Date();
 
-// Get the current day as an index (0-6)
 let currentDayIndex = currentDate.getDay();
 
-// Array of day names
-let dayNames = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+let dayNames = ["Sun,", "Mon,", "Tue,", "Wed,", "Thur,", "Fri,", "Sat,"];
 
-// Get the current day name
 let currentDayName = dayNames[currentDayIndex];
 
-// Update the element with the current day
 let todayElement = document.querySelector(".today-date");
 todayElement.textContent = currentDayName;
 
-// Get the hour and minute values
 let currentHour = currentDate.getHours();
 let currentMinute = currentDate.getMinutes();
 
-// Update the hour element
 let hourElement = document.querySelector(".hr-live-time");
 hourElement.textContent = currentHour;
 
-// Update the minute element
 let minuteElement = document.querySelector(".mins-live-time");
 minuteElement.textContent = currentMinute;
 
-// Update the seconds element
 const updateSecs = () => {
   // Get the current date and time
   let currentDate = new Date();
@@ -47,15 +38,11 @@ let newProfileBtn = document.querySelector(".new-profile");
 let overlay = document.querySelector(".overlay");
 
 newProfileBtn.addEventListener("click", () => {
-  overlay.style.display = "block";
+  overlay.style.display = "flex";
 });
 
-// Select the day and night icons
 let dayIcon = document.querySelector(".day-icon");
 let nightIcon = document.querySelector(".night-icon");
-let thecurrentHour = new Date().getHours();
-
-// Display the appropriate icon based on the current hour
 
 if (currentHour >= 5 && currentHour <= 20) {
   dayIcon.style.display = "block";
